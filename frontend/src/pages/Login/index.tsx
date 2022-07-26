3a9fe7c188e9088d828e90d6ce082300ae91f3c0
DiegoCapella <diegocapelladev@gmail.com>
2022-07-26 15:01:34 -0300

    feat: add page Login

--- frontend/src/pages/Login/index.tsx
+++ frontend/src/pages/Login/index.tsx
@@ -1,8 +1,46 @@
+import { Link } from 'react-router-dom'
+
+import AuthImg from 'assets/images/auth.png'
+import Logo from 'assets/images/logo.png'
+
 import * as S from './styles'
+import Heading from 'components/Heading'
+import InputField from 'components/TextField'
+import Button from 'components/Button'
 
 const Login = () => (
   <S.Wrapper>
-    <h1>Login</h1>
+    <S.BannerBlock>
+      <S.ImageBg src={AuthImg} />
+
+      <S.BannerContent>
+        <Link to="/">
+          <img src={Logo} alt="Logo Lexio" />
+        </Link>
+
+        <Heading>
+          Criação e gestão de contratos de forma eficiente e descomplicada!
+        </Heading>
+      </S.BannerContent>
+    </S.BannerBlock>
+
+    <S.FormWrapper>
+      <S.FormContent>
+        <Heading color="gray700">Bem vindo(a)!</Heading>
+
+        <S.FormSubtitle>
+          Crie, assine e gerencie contratos como nunca antes{' '}
+          <strong>Comece agora!</strong>
+        </S.FormSubtitle>
+      </S.FormContent>
+
+      <S.Form>
+        <InputField type="email" name="email" placeholder="exemple@email.com" />
+        <InputField type="password" name="password" placeholder="Senha" />
+
+        <Button>Login</Button>
+      </S.Form>
+    </S.FormWrapper>
   </S.Wrapper>
 )
 
--- frontend/src/pages/Login/styles.ts
+++ frontend/src/pages/Login/styles.ts
@@ -1,3 +1,86 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
 
-export const Wrapper = styled.main``
+export const Wrapper = styled.main`
+  display: grid;
+  grid-template-columns: 1fr;
+  height: 100vh;
+  padding: 4rem;
+
+  ${media.greaterThan('medium')`
+    grid-template-columns: 1fr 1fr;
+  `}
+
+  ${media.lessThan('medium')`
+    padding: 1rem;
+  `}
+`
+
+export const BannerBlock = styled.div`
+  position: relative;
+
+  ${media.lessThan('medium')`
+    display: none;
+  `}
+`
+export const ImageBg = styled.img`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  border-radius: 2rem;
+`
+
+export const BannerContent = styled.div`
+  ${({ theme }) => css`
+    color: ${theme.colors.white};
+    z-index: ${theme.layers.base};
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    position: relative;
+    padding: 10rem;
+
+    
+  `}
+
+  h2 {
+    margin-top: 4rem;
+    text-align: center;
+  }
+`
+
+export const FormWrapper = styled.div`
+  padding: 6.4rem;
+
+  ${media.lessThan('medium')`
+    padding: 1rem;
+  `}
+`
+
+export const FormSubtitle = styled.p`
+  ${({ theme }) => css`
+    color: ${theme.colors.gray700};
+    margin: ${theme.spacings.medium} auto;
+    font-size: ${theme.font.sizes.large};
+  `}
+`
+
+export const FormContent = styled.div`
+  margin-bottom: 8rem;
+  > h2 {
+    font-size: 3.6rem;
+  }
+`
+
+export const Form = styled.form`
+  max-width: 50rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: end;
+
+  > button {
+    width: fit-content;
+    align-self: flex-end;
+  }
+`
